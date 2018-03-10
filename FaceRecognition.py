c700dd0508c85030af683f71aec777a1034cb15a
SenuraFernando <senura.fernando@gmail.com>
2018-03-10 22:20:48 +0530

    Training code for the the classes

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -6,7 +6,7 @@ from keras.preprocessing import image
 # -----------------------------
 # opencv initialization
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('./HaarCascade/haarcascade_frontalface_default.xml')
 
 cap = cv2.VideoCapture(0)
 # -----------------------------
