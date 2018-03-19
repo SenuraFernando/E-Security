4db6e78dc52d54698bd1a7c72e5327102a3eeae0
SenuraFernando <senura.fernando@gmail.com>
2018-03-19 16:30:05 +0530

    Recognition code for the the classes

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -22,18 +22,19 @@ names = {
     1: 'Pasindu',
     2: 'Senura',
     3: 'Sheelu',
-
+#0: 'Senura',
+# 1: 'Isuru',
 }
-# classes = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'senura')
+# emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'senura')
 
 while (True):
     ret, img = cap.read()
     # img = cv2.imread('C:/Users/IS96273/Desktop/hababam.jpg')
 
-    #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    colour= cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #gray= cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    faces = face_cascade.detectMultiScale(colour, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
         # print(faces) #locations of detected faces
 
@@ -41,10 +42,11 @@ while (True):
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)  # draw rectangle to main image
 
         detected_face = img[int(y):int(y + h), int(x):int(x + w)]  # crop detected face
-        detected_face = cv2.cvtColor(detected_face, cv2.COLOR_BGR2GRAY)  # transform to gray scale
-        detected_face = cv2.resize(detected_face, (48, 48))  # resize to 48x48
+        #detected_face = cv2.cvtColor(detected_face, cv2.COLOR_BGR2GRAY)  # transform to gray scale
+        detected_face = cv2.resize(detected_face, (150, 150))  # resize to 48x48
 
         img_pixels = image.img_to_array(detected_face)
+        #img_pixels = np.expand_dims(img_pixels, axis=0)
         img_pixels = np.expand_dims(img_pixels, axis=0)
 
         img_pixels /= 255  # pixels are in scale of [0, 255]. normalize all pixels in scale of [0, 1]
@@ -54,10 +56,12 @@ while (True):
         # find max indexed array 0: angry, 1:disgust, 2:fear, 3:happy, 4:sad, 5:surprise, 6:neutral
         max_index = np.argmax(predictions[0])
 
-        names = names[max_index]
+        name = names[max_index]
+        #emotion = emotions[max_index]
+
 
         # write emotion text above rectangle
-        cv2.putText(img, names, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        cv2.putText(img, name, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 
     # process on detected face end
     # -------------------------
